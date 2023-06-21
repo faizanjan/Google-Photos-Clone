925b358f7c1f74393c6777714f7ab0e934babd77
Faizan Jan <faizanjan619@gmail.com>
2023-06-21 18:11:14 +0530

    Add some styling to the shharde photos

--- src/components/sharingComponents/SharedPhoto.jsx
+++ src/components/sharingComponents/SharedPhoto.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SharedPhoto = ({ photo, index, showSent, showCarousel, setActiveIndex }) => {
   return (
     <div
-      className="photo-container d-flex flex-column position-relative mx-2"
+      className="photo-container d-flex flex-column position-relative mx-2 border shadow rounded"
       style={{ margin: "20px 0" }}
       onClick={()=>{
         showCarousel()
@@ -11,7 +11,7 @@ const SharedPhoto = ({ photo, index, showSent, showCarousel, setActiveIndex }) =
       }}
     >
       <img src={photo.url} style={{ height: "250px" }} alt="google photo" />
-      <span className="text-muted">
+      <span className="text-muted p-1 ms-2">
         {showSent ? "To: " + photo.to : "From: " + photo.from}
       </span>
     </div>
