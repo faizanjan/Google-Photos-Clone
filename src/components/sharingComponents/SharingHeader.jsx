87d882393edcb5254b1a9807b165d8d151327ae5
Faizan Jan <faizanjan619@gmail.com>
2023-06-22 13:34:38 +0530

    Change the bg of active tab in sharng

--- src/components/sharingComponents/SharingHeader.jsx
+++ src/components/sharingComponents/SharingHeader.jsx
@@ -2,11 +2,33 @@ const SharingHeader = ({ showSent, setShowSent }) => {
   return (
     <>
       <ul className="nav nav-tabs my-3">
-        <li className="nav-item hover-pointer" onClick={() => setShowSent(true)}>
-          <span className={"nav-link fs-5" + (showSent ? " active text-primary" : " text-dark fw-light")}>Sent</span>
+        <li
+          className="nav-item hover-pointer"
+          onClick={() => setShowSent(true)}
+        >
+          <span
+            style={{ background: showSent ? "rgb(232,240,254)" : "" }}
+            className={
+              "nav-link fs-5" +
+              (showSent ? " active text-primary" : " text-dark fw-light")
+            }
+          >
+            Sent
+          </span>
         </li>
-        <li className="nav-item hover-pointer" onClick={() => setShowSent(false)}>
-          <span className={"nav-link fs-5" + (showSent ? " text-dark fw-light" : " active text-primary")}>Received</span>
+        <li
+          className="nav-item hover-pointer"
+          onClick={() => setShowSent(false)}
+        >
+          <span
+            style={{ background: !showSent ? "rgb(232,240,254)" : "" }}
+            className={
+              "nav-link fs-5" +
+              (showSent ? " text-dark fw-light" : " active text-primary")
+            }
+          >
+            Received
+          </span>
         </li>
       </ul>
     </>
