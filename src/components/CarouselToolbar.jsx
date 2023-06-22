76ef0cefdc4a32cf9ac26065f17ed14ac4fa6e19
Faizan Jan <faizanjan619@gmail.com>
2023-06-22 13:31:29 +0530

    Replace the alert with snackbar when user tries to  share a photo to non-existent user

--- src/components/CarouselToolbar.jsx
+++ src/components/CarouselToolbar.jsx
@@ -20,6 +20,7 @@ import ToolTip from "./secondary_components/ToolTip.jsx";
 import MoreInfo from "./secondary_components/MoreInfo.jsx";
 import SharingModal from "./sharingComponents/SharingModal.jsx";
 import AddToAlbumModal from "./albumComponents/AddToAlbumModal.jsx";
+import CustomizedSnackbars from "./secondary_components/Snackbar.jsx";
 
 const CarouselToolbar = ({ photoIndex, setActiveIndex, lastIndex }) => {
   let { pathname } = useLocation();
@@ -27,22 +28,28 @@ const CarouselToolbar = ({ photoIndex, setActiveIndex, lastIndex }) => {
   let { currentUser } = useAuth();
   let [showAddToAlbum, setShowAddToAlbum] = useState(false);
   let [showSharing, setShowSharing] = useState(false);
+  let [snackbar, setSnackbar] = useState({});
+  let [showSnackbar, setShowSnackbar] = useState(false);
 
   let key = getStateKey(pathname);
   let photo = useSelector((state) => state[key][photoIndex]);
   let dispatch = useDispatch();
   const [isFav, setIsFav] = useState(Boolean(photo?.isFavourite));
-  
+
   useEffect(() => {
-    if(photo)setIsFav(Boolean(photo.isFavourite));
+    if (photo) setIsFav(Boolean(photo.isFavourite));
   }, [photo]);
-  
-  if(!photo) return ;
-  
-  const handleSharing = (receipientId, receipientEmail)=>{
-    if(!receipientId) alert('User does not exist')
-    sharePhotos(receipientId, receipientEmail, currentUser, [photo])
-  }
+
+  if (!photo) return;
+
+  const handleSharing = (receipientId, receipientEmail) => {
+    if (!receipientId) {
+      setSnackbar({ severity: "error", message: "User does not exist" });
+      setShowSnackbar(true);
+      return;
+    }
+    sharePhotos(receipientId, receipientEmail, currentUser, [photo]);
+  };
 
   const handleDelete = async (event, path, docId) => {
     event.stopPropagation();
@@ -108,91 +115,105 @@ const CarouselToolbar = ({ photoIndex, setActiveIndex, lastIndex }) => {
   };
 
   return (
-    <div className="carousel-toolbar bg-dark py-4 d-flex flex-row justify-content-between align-items-center">
-      <i
-        className="fa-solid fa-arrow-left text-light fs-3 ms-4 hover-pointer"
-        onClick={(e) => {
-          setShowCarousel(false);
-        }}
-      ></i>
-      <div className="carousel-tools me-5">
-        <ToolTip tooltip="Share">
-          <i
-            style={pathname === "/home/bin" ? { display: "none" } : {}}
-            className="mx-3 hover-pointer text-light fa-solid fa-share-nodes"
-            onClick={()=>setShowSharing(true)}
-          ></i>
-        </ToolTip>
-
-        <ToolTip tooltip="Edit">
-          <i
-            style={pathname === "/home/bin" ? { display: "none" } : {}}
-            className="mx-3 hover-pointer text-light fa-solid fa-sliders"
-          ></i>
-        </ToolTip>
-
-        <span
-          style={pathname !== "/home/bin" ? { display: "none" } : {}}
-          className="delete-from-bin text-light mx-3 hover-pointer "
-          onClick={(e) => handleDelete(e, photo.path, photo.id)}
-        >
-          <i className="mx-3 text-light fa-solid fa-trash"></i>
-          Delete
-        </span>
-
-        <span
-          style={pathname !== "/home/bin" ? { display: "none" } : {}}
-          className="delete-from-bin text-light mx-3 hover-pointer "
+    <>
+      <div className="carousel-toolbar bg-dark py-4 d-flex flex-row justify-content-between align-items-center">
+        <i
+          className="fa-solid fa-arrow-left text-light fs-3 ms-4 hover-pointer"
           onClick={(e) => {
-            handleRestore(e, photo.id);
+            setShowCarousel(false);
           }}
-        >
-          <i className="mx-3 fa-solid fa-clock-rotate-left"></i>
-          Restore
-        </span>
+        ></i>
+        <div className="carousel-tools me-5">
+          <ToolTip tooltip="Share">
+            <i
+              style={pathname === "/home/bin" ? { display: "none" } : {}}
+              className="mx-3 hover-pointer text-light fa-solid fa-share-nodes"
+              onClick={() => setShowSharing(true)}
+            ></i>
+          </ToolTip>
 
-        <ToolTip tooltip="Zoom">
-          <i className="mx-3 hover-pointer text-light fa-solid fa-magnifying-glass-plus"></i>
-        </ToolTip>
-        <ToolTip tooltip="Info">
-          <i className="mx-3 hover-pointer text-light fa-solid fa-circle-info"></i>
-        </ToolTip>
-        <ToolTip tooltip="Favourite">
-          <i
-            style={pathname === "/home/bin" ? { display: "none" } : {}}
-            className={
-              "mx-3 hover-pointer text-light fa-star" +
-              (isFav ? " fa-solid" : " fa-regular")
-            }
-            onClick={() => {
-              toggleFavourite(photo.id, photo.isFavourite);
-            }}
-          ></i>
-        </ToolTip>
-        <ToolTip tooltip="Delete">
-          <i
-            style={pathname === "/home/bin" ? { display: "none" } : {}}
-            className="mx-3 hover-pointer text-light fa-solid fa-trash-can"
+          <ToolTip tooltip="Edit">
+            <i
+              style={pathname === "/home/bin" ? { display: "none" } : {}}
+              className="mx-3 hover-pointer text-light fa-solid fa-sliders"
+            ></i>
+          </ToolTip>
+
+          <span
+            style={pathname !== "/home/bin" ? { display: "none" } : {}}
+            className="delete-from-bin text-light mx-3 hover-pointer "
+            onClick={(e) => handleDelete(e, photo.path, photo.id)}
+          >
+            <i className="mx-3 text-light fa-solid fa-trash"></i>
+            Delete
+          </span>
+
+          <span
+            style={pathname !== "/home/bin" ? { display: "none" } : {}}
+            className="delete-from-bin text-light mx-3 hover-pointer "
             onClick={(e) => {
-              addToTrash(e, photo.id);
+              handleRestore(e, photo.id);
             }}
-          ></i>
-        </ToolTip>
-        {pathname !== "/home/bin" && <MoreInfo photo={photo} currentUser={currentUser} setShowAddToAlbum={setShowAddToAlbum}/>}
+          >
+            <i className="mx-3 fa-solid fa-clock-rotate-left"></i>
+            Restore
+          </span>
+
+          <ToolTip tooltip="Zoom">
+            <i className="mx-3 hover-pointer text-light fa-solid fa-magnifying-glass-plus"></i>
+          </ToolTip>
+          <ToolTip tooltip="Info">
+            <i className="mx-3 hover-pointer text-light fa-solid fa-circle-info"></i>
+          </ToolTip>
+          <ToolTip tooltip="Favourite">
+            <i
+              style={pathname === "/home/bin" ? { display: "none" } : {}}
+              className={
+                "mx-3 hover-pointer text-light fa-star" +
+                (isFav ? " fa-solid" : " fa-regular")
+              }
+              onClick={() => {
+                toggleFavourite(photo.id, photo.isFavourite);
+              }}
+            ></i>
+          </ToolTip>
+          <ToolTip tooltip="Delete">
+            <i
+              style={pathname === "/home/bin" ? { display: "none" } : {}}
+              className="mx-3 hover-pointer text-light fa-solid fa-trash-can"
+              onClick={(e) => {
+                addToTrash(e, photo.id);
+              }}
+            ></i>
+          </ToolTip>
+          {pathname !== "/home/bin" && (
+            <MoreInfo
+              photo={photo}
+              currentUser={currentUser}
+              setShowAddToAlbum={setShowAddToAlbum}
+            />
+          )}
+        </div>
+
+        <SharingModal
+          show={showSharing}
+          onHide={() => setShowSharing(false)}
+          sharePhotoWith={handleSharing}
+        />
+
+        <AddToAlbumModal
+          show={showAddToAlbum}
+          onHide={() => setShowAddToAlbum(false)}
+          photo={photo}
+        />
       </div>
-
-      <SharingModal
-        show={showSharing}
-        onHide={() => setShowSharing(false)}
-        sharePhotoWith ={handleSharing}
+      <CustomizedSnackbars
+        severity={snackbar.severity}
+        message={snackbar.message}
+        open={showSnackbar}
+        setOpen={setShowSnackbar}
       />
-
-      <AddToAlbumModal
-        show={showAddToAlbum}
-        onHide={()=> setShowAddToAlbum(false)}
-        photo={photo}
-      />
-    </div>
+    </>
   );
 };
 
