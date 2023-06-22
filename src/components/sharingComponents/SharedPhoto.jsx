d062ad38a27dd6b2a470dfa2053dcf897430391b
Faizan Jan <faizanjan619@gmail.com>
2023-06-22 11:07:37 +0530

    Change the UI of sharing tab to make it neater

--- src/components/sharingComponents/SharedPhoto.jsx
+++ src/components/sharingComponents/SharedPhoto.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import { useState } from "react";
 
-const SharedPhoto = ({ photo, index, showSent, showCarousel, setActiveIndex }) => {
+const SharedPhoto = ({
+  photo,
+  index,
+  showSent,
+  showCarousel,
+  setActiveIndex,
+}) => {
+  const [showDetails, setShowDetails] = useState(false);
   return (
     <div
-      className="photo-container d-flex flex-column position-relative mx-2 border shadow rounded"
-      style={{ margin: "20px 0" }}
-      onClick={()=>{
-        showCarousel()
-        setActiveIndex(index)
+      className={
+        "d-flex flex-column position-relative m-2 mb-3 rounded-3" +
+        (showDetails ? " shadow " : "")
+      }
+      style={{ margin: "20px 0", transition: 'all 0.5s' }}
+      onClick={() => {
+        showCarousel();
+        setActiveIndex(index);
       }}
+      onMouseOver={() => setShowDetails(true)}
+      onMouseOut={() => setShowDetails(false)}
     >
       <img src={photo.url} style={{ height: "250px" }} alt="google photo" />
-      <span className="text-muted p-1 ms-2">
+
+      <span
+        className="text-secondary p-2 ms-2"
+        style={{
+          position: "relative",
+          top: showDetails ? "0" : "-50px",
+          opacity: showDetails ? "1" : 0,
+          transition: 'all 0.3s'
+        }}
+      >
         {showSent ? "To: " + photo.to : "From: " + photo.from}
       </span>
     </div>
--- src/components/sharingComponents/SharingHeader.jsx
+++ src/components/sharingComponents/SharingHeader.jsx
@@ -1,35 +1,15 @@
-
 const SharingHeader = ({ showSent, setShowSent }) => {
   return (
-    <ul id="sharing-menu">
-      <li
-        onClick={() => setShowSent(false)}
-        style={
-          !showSent
-            ? {
-                color: "#0d6efd",
-                borderBottom: "2px solid #0d6efd",
-              }
-            : {}
-        }
-      >
-        Received
-      </li>
-
-      <li
-        onClick={() => setShowSent(true)}
-        style={
-          showSent
-            ? {
-                color: "#0d6efd",
-                borderBottom: "2px solid #0d6efd",
-              }
-            : {}
-        }
-      >
-        Sent
-      </li>
-    </ul>
+    <>
+      <ul className="nav nav-tabs my-3">
+        <li className="nav-item hover-pointer" onClick={() => setShowSent(true)}>
+          <span className={"nav-link fs-5" + (showSent ? " active text-primary" : " text-dark fw-light")}>Sent</span>
+        </li>
+        <li className="nav-item hover-pointer" onClick={() => setShowSent(false)}>
+          <span className={"nav-link fs-5" + (showSent ? " text-dark fw-light" : " active text-primary")}>Received</span>
+        </li>
+      </ul>
+    </>
   );
 };
 
