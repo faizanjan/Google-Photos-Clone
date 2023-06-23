6d32b561b7fd449fa8fca4349e1049142f361949
Faizan Jan <faizanjan619@gmail.com>
2023-06-23 11:25:53 +0530

    Make an .env file for firebase credentials and use it in firebase.config instead of actual credentials. add .env to .gitignore

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,18 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyDSJMxSEE-3LScmOeG7lZsVBnXOsYr0Re4",
-  // authDomain: "photos-3aa17.firebaseapp.com",
-  // projectId: "photos-3aa17",
-  // storageBucket: "photos-3aa17.appspot.com",
-  // messagingSenderId: "218609000082",
-  // appId: "1:218609000082:web:ecb306606300bafe91504b",
-  apiKey: "AIzaSyBvM_d-JLqaIDAAk3iJ3PIemvaqs9nhFiQ",
-  authDomain: "photos-32238.firebaseapp.com",
-  projectId: "photos-32238",
-  storageBucket: "photos-32238.appspot.com",
-  messagingSenderId: "595175892024",
-  appId: "1:595175892024:web:9c9575826c86e23fd372cd"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
